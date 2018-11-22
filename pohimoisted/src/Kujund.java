709fb32eb2269e4e514f94f6e2b732e7ae59ff76
mairisaar <mairi.saar@khk.ee>
2018-11-22 09:21:00 +0200

    Pindala ümardamine teisaldatud andmete väljastamise meetodisse.

---
+++ pohimoisted/src/Kujund.java
@@ -0,0 +1,23 @@
+public class Kujund {
+    // tüübi omadused - klassi muutujad
+    public String nimetus;
+    public Double pindala;
+    public Boolean nurkadeOlemasolu;
+    public Integer nurkadeArv;
+
+    // klassi konstruktor
+    public Kujund(String nimi) {
+        this.nimetus = nimi;
+    }
+
+    //tüübi tegevused - klassi meetodid
+    public void arvutaPindala() {
+        System.out.println("Kujund klassi arvutaPindala meetod");
+    }
+
+    public void valjastaKirjeldus() {
+        System.out.println(this.nimetus);
+        this.arvutaPindala();
+        System.out.println();
+    }
+}
