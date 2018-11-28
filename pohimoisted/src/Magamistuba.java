2369224a7c5327f3074f026159ee5eb7520c3557
mairisaar <mairi.saar@khk.ee>
2018-11-28 09:42:14 +0200

    Loodud klass Magamistuba.

---
+++ pohimoisted/src/Magamistuba.java
@@ -0,0 +1,15 @@
+public class Magamistuba {
+    public static void main(String[] args) {
+        Sein sein1 = new Sein("põhi");
+        Sein sein2 = new Sein("lõuna");
+        Sein sein3 = new Sein("lääs");
+        Sein sein4 = new Sein("ida");
+
+        System.out.println(sein1.getSuund());
+        System.out.println(sein2.getSuund());
+        System.out.println(sein3.getSuund());
+        System.out.println(sein4.getSuund());
+    }
+
+
+}
