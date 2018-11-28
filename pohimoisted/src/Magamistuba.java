897b77295e81566d8da9c83ff3c9031bd2a4844a
mairisaar <mairi.saar@khk.ee>
2018-11-28 09:46:03 +0200

    Magamistoale lisatud lagi.

--- pohimoisted/src/Magamistuba.java
+++ pohimoisted/src/Magamistuba.java
@@ -4,11 +4,14 @@ public class Magamistuba {
         Sein sein2 = new Sein("lõuna");
         Sein sein3 = new Sein("lääs");
         Sein sein4 = new Sein("ida");
+        Lagi lagi = new Lagi(2.5, "valge");
 
         System.out.println(sein1.getSuund());
         System.out.println(sein2.getSuund());
         System.out.println(sein3.getSuund());
         System.out.println(sein4.getSuund());
+        System.out.println("Lae kõrgus: " + lagi.getKorgus());
+        System.out.println("Lae värvus: " + lagi.getVarv());
     }
 
 
