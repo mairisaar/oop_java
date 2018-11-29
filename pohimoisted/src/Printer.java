2456d417641d1554a6ec35ab78972a8394b52ffe
mairisaar <mairi.saar@khk.ee>
2018-11-29 09:34:43 +0200

    Muudetud Printer tüübi konstruktorit.

--- pohimoisted/src/Printer.java
+++ pohimoisted/src/Printer.java
@@ -2,30 +2,40 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 
 public class Printer {
     private Integer tooneriTase;
-    private Integer paberiArv;
+    public Integer paberiArv;
     private Boolean kasKahepoolne;
 
-    public Printer(Integer tooneriTase, Integer paberiArv, Boolean kasKahepoolne) {
+    public Printer(Integer tooneriTase, Boolean kasKahepoolne) {
         this.tooneriTase = tooneriTase;
-        this.paberiArv = paberiArv;
         this.kasKahepoolne = kasKahepoolne;
+        this.paberiArv = 100;
     }
 
     public void prindi(Integer lehed) {
         System.out.println("Paberite arv enne printimist: " + this.paberiArv);
-        
+
         if (paberiArv >= lehed) {
             this.paberiArv -= lehed;
             System.out.println("Paberite arv pärast printimist: " + this.paberiArv);
         } else {
-            System.out.println("Prinditi " + paberiArv + " lehte. Printimata jäi " + (lehed - paberiArv) + " lehte. Lisa printerisse paberit!");
+            if (lehed - paberiArv == 1) {
+                System.out.println("Prinditi " + paberiArv + " lehte. Printimata jäi " + (lehed - paberiArv) + " leht. Lisa printerisse paberit!");
+            } else {
+                System.out.println("Prinditi " + paberiArv + " lehte. Printimata jäi " + (lehed - paberiArv) + " lehte. Lisa printerisse paberit!");
+            }
         }
 
     }
 
     public void lisaTooner(Integer tooner) {
         System.out.println("Tooneri tase enne täitmist on: " + this.tooneriTase + "%");
-        this.tooneriTase += tooner;
-        System.out.println("Uus tooneri tase on: " + this.tooneriTase + "%");
+        if (tooneriTase + tooner <= 100) {
+            this.tooneriTase += tooner;
+            System.out.println("Uus tooneri tase on: " + this.tooneriTase + "%");
+        } else {
+            System.out.println("Tooner on täidetud 100%. Üle jäi " + ((tooneriTase + tooner) - 100) + "%");
+            this.tooneriTase = 100;
+        }
+
     }
 }
--- pohimoisted/src/PrinterTest.java
+++ pohimoisted/src/PrinterTest.java
@@ -1,9 +1,11 @@
 public class PrinterTest {
     public static void main(String[] args) {
-        Printer overall = new Printer(32, 12, true);
+        Printer overall = new Printer(50, true);
 
-        overall.prindi(3);
-        overall.lisaTooner(12);
+        overall.paberiArv = 45;
+
+        overall.prindi(22);
+        overall.lisaTooner(51);
 
     }
 
